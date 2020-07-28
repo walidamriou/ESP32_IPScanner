d02b00331d2379050953ce316c274d625cceb8b5
Walid Amriou <walidamriou@gmail.com>
2020-07-28 20:39:05 +0200

    add src/main.c

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/* 
+ESP32 OTA Update via HTTPS 
+Implement Esperssif ESP32 OTA (Over-The-Air) updates via HTTPS with the ESP-IDF framework.
+Developed by: Walid Amriou
+www.walidamriou.com
+
+This work is licensed under:
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+
+Last update: July 2020
+*/
+
+#include <wifi.h>
+#include <esp_system.h>
+
+void app_main() {
+    wifi_init();
+    printf("wifi init done.\n");
+    wifi_connect();
+    printf("Connected to wifi network\n");
+}
